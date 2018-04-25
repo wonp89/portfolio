dca97810b89203cc51e24c55ca8a0b70f0147c95
Won <wonpark89@gmail.com>
2018-04-24 20:51:32 -0700

    trying to deploy again without morgan

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var logger = require('morgan');
+// var logger = require('morgan');
 var path = require('path')
 
 var app = express();
 
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
