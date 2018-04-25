61c862d96561a3578c6a2d61084abcc5bdc39269
Won <wonpark89@gmail.com>
2018-04-24 20:47:25 -0700

    imges is being imported for production

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
 const autoprefixer = require('autoprefixer');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-const webpack = require('webpack'); //added
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-    devtool: 'cheap-module-source-map', //changed
+    devtool: 'cheap-module-source-map',
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -28,61 +29,67 @@ module.exports = {
                 exclude: /node_modules/,
                 use: ExtractTextPlugin.extract({
                     fallback: 'style-loader',
-                use: [
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            importLoaders: 1,
-                            modules: true,
-                            localIdentName: '[name]__[local]__[hash:base64:5]'
-                        }
-                     },
-                     {
-                         loader: 'postcss-loader',
-                         options: {
-                             ident: 'postcss',
-                             plugins: () => [
-                                 autoprefixer({
-                                     browsers: [
-                                        "> 1%",
-                                        "last 2 versions"
-                                     ]
-                                 })
-                             ]
-                         }
-                      },
-                      { loader: 'sass-loader'}
-                ]
-            })
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                importLoaders: 1,
+                                modules: true,
+                                localIdentName: '[name]__[local]__[hash:base64:5]'
+                            }
+                        },
+                        {
+                            loader: 'postcss-loader',
+                            options: {
+                                ident: 'postcss',
+                                plugins: () => [
+                                    autoprefixer({
+                                        browsers: [
+                                            "> 1%",
+                                            "last 2 versions"
+                                        ]
+                                    })
+                                ]
+                            }
+                        },
+                        { loader: 'sass-loader' }
+                    ]
+                })
             },
             {
                 test: /\.(png|jpe?g|gif)$/,
                 loader: 'url-loader?limit=8000&name=images/[name].[ext]'
-            }
+            },
         ]
     },
     plugins: [
+        //for using media query
         new webpack.LoaderOptionsPlugin({
             test: /\.(css|scss|sass)$/,
             options: {
-              postcss: [
-                require('postcss-import')(),
-                require('postcss-url')(),
-                require('postcss-cssnext')({
-                  browsers: 'last 2 versions'
-                })
-              ],
-              context: path.resolve(__dirname, "src")
+                postcss: [
+                    require('postcss-import')(),
+                    require('postcss-url')(),
+                    require('postcss-cssnext')({
+                        browsers: 'last 2 versions'
+                    })
+                ],
+                context: path.resolve(__dirname, "src")
             }
-          }),
+        }),
         new HtmlWebpackPlugin({
             template: __dirname + '/src/index.html',
             filename: 'index.html',
             inject: 'body'
         }),
+        //have css file linked inside html
         new ExtractTextPlugin({
             filename: "index.css"
         }),
-        new webpack.optimize.UglifyJsPlugin() //added to optimize js
+        //copy images to dist folder
+        new CopyWebpackPlugin([
+            { from: 'src/assets/images', to: 'src/assets/images' }
+        ]),
+        new webpack.optimize.UglifyJsPlugin()
     ]
 };
