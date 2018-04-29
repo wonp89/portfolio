7569cb9fd798df9c1a14d4d7f8e71040898257dc
Won <wonpark89@gmail.com>
2018-04-28 23:05:42 -0700

    connect-school is added

--- src/components/PortfolioList.js
+++ src/components/PortfolioList.js
@@ -14,6 +14,23 @@ class PortfolioList extends Component {
                             <h3>SUSHI FIELD</h3>
                             <p className={classes.dotDecoration}>•</p>
                             <Col md={6} className={classes.firstImageContainer}>
+                                <img src="../src/assets/images/portfolio6-1.png" />
+                            </Col>
+                            <Col md={6} >
+                                <img src="../src/assets/images/portfolio6-2.png" />
+                            </Col>
+                            <p className={classes.dotDecoration}>•</p>
+                            <div className={classes.linkContainer}>
+                                <a href="https://github.com/wonp89/connect-school" target="_blank">SOURCE CODE</a><span> | </span><a href="http://connect-school.herokuapp.com" target="_blank">VIEW WEBSITE</a>
+                            </div>
+                            <p className={classes.technologies}> NodeJS, ExpressJS, ReactJS, Redux, MongoDB, PassportJS, Sendgrid, Materialize-css, Redux-Form</p>
+                            <p> This is the longterm project which I’m focusing on connecting the students in Vancouver. The feature that my website has at the moment is that the users can create events by selecting schools they have enrolled. As soon as users joined the events, they will get confirmation emails with the event detail. Users will also have a separate page that displays the events that they have joined. If the date of the event has passed, the event post will be automatically disabled and remained as “expired event”. Users can also sign out from the events and their names will be removed from the lists of participants. More features will be added in the future.</p>
+                        </Row>
+
+                        <Row className={classes.portfolioList}>
+                            <h3>SUSHI FIELD</h3>
+                            <p className={classes.dotDecoration}>•</p>
+                            <Col md={6} className={classes.firstImageContainer}>
                                 <img src="../src/assets/images/portfolio5-1.png" />
                             </Col>
                             <Col md={6} >
